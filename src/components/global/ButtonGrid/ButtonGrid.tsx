0be149d63cdc11c569ee23de4d36b2b319a3f26f
sieon <sieon.eve@gmail.com>
2024-11-19 14:52:22 +0900

    feat: button grid 초안

---
+++ src/components/global/ButtonGrid/ButtonGrid.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+interface ButtonGridProps {
+row : number;
+col : number;
+buttonName: string;
+selected?:boolean;
+}
+
+const ButtonGrid = styled.div`    
+    margin: 0 auto;
+`;
+
+const ButtonStyle = styled.button`
+    
+`;
+export function ButtonGird({row, col, buttonName, selected}:ButtonGridProps){
+return();
+}
