c1c1a131adb130c0a2f5a89a9af8a805c5b4e306
sieon <sieon.eve@gmail.com>
2024-11-26 00:05:14 +0900

    feat: Text 컴포넌트 생성

--- src/components/global/texts/text/Text.styles.ts
+++ src/components/global/texts/text/Text.styles.ts
@@ -1,7 +1,7 @@
-import styled, { css } from 'styled-components';  // css를 import
+import styled, { css } from 'styled-components';  // css와 styled-components import
 import { typography } from "../../../../style/typography";  // typography import
 
-// Props로 받을 컬러와 typo 타입 정의
+// StyledText 컴포넌트에서 사용할 Props 타입 정의
 interface StyledTextProps {
   color?: string;
   typo: keyof typeof typography;
@@ -10,7 +10,7 @@ interface StyledTextProps {
 // 스타일 정의
 export const StyledText = styled.span<StyledTextProps>`
   ${({ typo }) => css`
-    ${typography[typo]}  // typography에서 해당 typo 스타일 적용
+    ${typography[typo]}  // typography에서 typo에 해당하는 스타일 적용
   `}
-  color: ${({ color }) => color || 'inherit'};  // color가 있으면 그 값, 없으면 'inherit'
+  color: ${({ color }) => color || 'inherit'};  // color가 있으면 그 값, 없으면 'inherit'로 설정
 `;
