db98314755f2b8ed1d816c0fd7c20024cf6aa78c
myoungjinGo-FE <cokemania2099@gmail.com>
2024-11-26 17:14:28 +0900

    fix: CustomButton, Input, ProgressBlock 촘촘한 수정

--- src/components/global/input/CustomInput/CustomInput.styles.ts
+++ src/components/global/input/CustomInput/CustomInput.styles.ts
@@ -42,31 +42,30 @@ export const InputWrapper = styled.div<{
   align-items: center;
   width: 100%;
   background-color: ${(props) =>
-    props.$disabled ? colors.gray100 : "transparent"};
+    props.$disabled ? colors.gray400 : "transparent"};
   ${({ $variant, $error, $disabled }) =>
     $variant === "outlined"
       ? css`
-          border-radius: 5px;
+          border-radius: 10px;
           border: ${$disabled
             ? "none"
-            : `1px solid ${$error ? colors.red200 : colors.gray300}`};
-          padding: 0px 10px;
+            : `1px solid ${$error ? colors.red100 : colors.gray300}`};
           height: 40px;
 
           &:focus-within {
-            border-color: ${$error ? colors.red200 : colors.blue200};
-            box-shadow: 0 0 0 3px ${$error ? colors.red100 : colors.blue100};
+            border-color: ${$error ? colors.red100 : colors.blue200};
+            box-shadow: 0 0 0 1px ${$error ? colors.red100 : colors.blue100};
           }
         `
       : css`
           border: none;
-          border-bottom: 1px solid ${$error ? colors.red200 : colors.gray300};
+          border-bottom: 1px solid ${$error ? colors.red100 : colors.gray300};
           border-radius: 0;
           padding: 5px 10px;
           height: 32px;
 
           &:focus-within {
-            border-bottom-color: ${$error ? colors.red200 : colors.blue200};
+            border-bottom-color: ${$error ? colors.red100 : colors.blue200};
           }
         `}
 `;
@@ -77,6 +76,7 @@ export const StyledInput = styled.input<{ $error: boolean }>`
   outline: none;
   background-color: transparent;
   font-size: 14px;
+  padding-left: 10px;
 
   &::placeholder {
     color: #9ca3af;
@@ -84,7 +84,7 @@ export const StyledInput = styled.input<{ $error: boolean }>`
   }
 
   &:disabled {
-    color: ${colors.gray300};
+    color: ${colors.black};
     cursor: not-allowed;
     font-size: 12px;
   }
@@ -100,32 +100,30 @@ export const SuffixContainer = styled.div<{
   ${({ $variant, $error }) =>
     $variant === "outlined"
       ? css`
-          border-radius: 20px;
-          border: 2px solid ${$error ? colors.red300 : colors.gray200};
           padding: 8px 16px;
           height: 32px;
 
           &:focus {
-            border-color: ${$error ? colors.red300 : colors.blue200};
+            border-color: ${$error ? colors.red100 : colors.blue200};
             box-shadow: 0 0 0 3px
               ${$error ? "rgba(239, 68, 68, 0.1)" : "rgba(79, 70, 229, 0.1)"};
           }
         `
       : css`
           border: none;
-          border-bottom: 2px solid ${$error ? colors.red300 : colors.gray200};
+          border-bottom: 2px solid ${$error ? colors.red100 : colors.gray200};
           border-radius: 0;
           padding: 4px 0;
           height: 24px;
 
           &:focus {
-            border-bottom-color: ${$error ? colors.red300 : colors.blue200};
+            border-bottom-color: ${$error ? colors.red100 : colors.blue200};
           }
         `}
 `;
 
 export const Message = styled.p<{ $error?: boolean }>`
   margin-top: 4px;
-  color: ${(props) => (props.$error ? colors.red300 : "#6b7280")};
+  color: ${(props) => (props.$error ? colors.red100 : "#6b7280")};
   animation: ${fadeIn} 0.3s ease-in-out;
 `;
