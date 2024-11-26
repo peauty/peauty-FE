720f4739bac15e42f595e28bc9eaaf05dc7927b8
jissssu <leeland21@naver.com>
2024-11-27 01:09:29 +0900

    feat: DropDown 컴포넌트 생성

---
+++ src/components/global/button/DropButton/DropButton.stories.tsx
@@ -0,0 +1,39 @@
+// DropButton.stories.tsx
+import { Meta, StoryObj } from "@storybook/react";
+import DropButton, { DropButtonProps } from "./DropButton";
+
+const meta: Meta<typeof DropButton> = {
+  title: "Components/DropButton",
+  component: DropButton,
+  argTypes: {
+    onSelect: { action: "item selected" }, 
+    isActive: { control: "boolean" },
+    selectedValue: { control: "text" },
+    placeholder: { control: "text" },
+  },
+};
+
+export default meta;
+type Story = StoryObj<DropButtonProps>;
+
+export const Default: Story = {
+  args: {
+    isActive: false,
+    placeholder: "옵션을 선택하세요",
+    selectedValue: "",
+    options: ["옵션1", "옵션2", "옵션3", "옵션4"], // 드롭다운 항목
+    onSelect: (value) => console.log("선택된 값:", value),
+  },
+};
+
+
+
+export const ItemSelected: Story = {
+  args: {
+    isActive: false,
+    placeholder: "옵션을 선택하세요",
+    selectedValue: "옵션2",
+    options: ["옵션1", "옵션2", "옵션3", "옵션4"],
+    onSelect: (value) => console.log("선택된 값:", value),
+  },
+};
