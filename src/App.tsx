7803f21b2fe0f066968fac44037cc4f78998d8be
ZUITOPIA <korhwngml123@naver.com>
2024-11-26 14:57:27 +0900

    feat: router 설정 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import QueryProvider from './provider/query-provider';
-import GlobalStyle from './style/global-style';
+import { Outlet } from "react-router-dom";
 
 export default function App() {
-  return (
-    <QueryProvider>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          {/* <Route path="/" element={<Main />} /> 이런식으로 작성*/}
-        </Routes>
-      </BrowserRouter>
-    </QueryProvider>
-  );
+  return <Outlet />;
 }
---
+++ src/pages/my-page/pet/edit/index.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Style = {
+  Wrapper: styled.div`
+    width: 200px;
+    height: 200px;
+    border: 1px solid red;
+  `,
+};
