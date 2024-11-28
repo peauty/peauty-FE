7044f15d79fb5e2abc5e4368a006fb132f40a467
ZUITOPIA <korhwngml123@naver.com>
2024-11-28 21:32:34 +0900

    style: 라디오 버튼 스타일 변경

--- src/components/global/button/RadioSelectButton/RadioSelectButton.style.ts
+++ src/components/global/button/RadioSelectButton/RadioSelectButton.style.ts
@@ -5,20 +5,20 @@ export const GridWrapper = styled.div`
   display: grid;
   grid-template-rows: repeat(auto-fit, minmax(40px, 1fr));
   grid-template-columns: repeat(auto-fit, minmax(80px, 1fr));
-  gap: 8px;
   margin: 0 auto;
   width: 100%;
+  border-radius: 10px;
+  background-color: ${colors.gray400};
 `;
 
 export const ButtonStyle = styled.button<{ selected: boolean }>`
   padding: 8px 16px;
   border: 1px solid
-    ${({ selected }) => (selected ? `${colors.blue100}` : `${colors.gray200}`)};
-  background-color: ${({ selected }) =>
-    selected ? `${colors.blue300}` : `${colors.white}`};
+    ${({ selected }) => (selected ? `${colors.blue100}` : `${colors.gray400}`)};
+  background-color: ${({ selected }) => (selected ? `${colors.blue300}` : 0)};
   color: ${({ selected }) =>
     selected ? `${colors.blue100}` : `${colors.gray200}`};
-  border-radius: 5px;
+  border-radius: 10px;
   cursor: pointer;
   transition: all 0.3s;
 `;
