710690247cb478accdc859a0c144344511617a25
ZUITOPIA <korhwngml123@naver.com>
2024-11-29 16:17:46 +0900

    style: layout 안에 wrapper 로 한번 더 감싸기

--- src/pages/my-page/pet/edit/index.tsx
+++ src/pages/my-page/pet/edit/index.tsx
@@ -5,6 +5,7 @@ import {
   DropButton,
   Layout,
   MultiSelectButton,
+  Wrapper,
 } from "../../../../components";
 import { MultiSelectButtonProps } from "../../../../components/global/button/MultiSelectButton/MultiSelectButton";
 import { Text } from "../../../../components/global/texts/Text";
@@ -87,119 +88,121 @@ export default function Edit() {
   return (
     <>
       <Layout>
-        <ProfileImg
-          width="120px"
-          height="120px"
-          src="/svg/default-profile.svg"
-          alt="프로필"
-          onClick={handleEditProfile}
-        />
+        <Wrapper>
+          <ProfileImg
+            width="120px"
+            height="120px"
+            src="/svg/default-profile.svg"
+            alt="프로필"
+            onClick={handleEditProfile}
+          />
 
-        <Style.EditPageWrapper>
-          <Style.Wrapper>
+          <Style.EditPageWrapper>
+            <Style.Wrapper>
+              <CustomInput
+                error=""
+                hint=""
+                label="이름"
+                placeholder="이름을 입력해주세요"
+                variant="outlined"
+              />
+            </Style.Wrapper>
+
+            <DropButton
+              label="견종"
+              placeholder="견종을 선택해주세요"
+              options={dogBreeds}
+              onSelect={handleBreedSelect}
+            />
+            <Text typo="subtitle300">
+              성별
+              <RadioSelectButton
+                {...(Gender.args as RadioSelectButtonProps)}
+                selectedIndex={0}
+                onSelect={handleGenderSelect}
+              />
+            </Text>
+
+            <Text typo="subtitle300">
+              분류
+              <RadioSelectButton
+                {...(Default.args as RadioSelectButtonProps)}
+                selectedIndex={0}
+                onSelect={handleSizeSelect}
+              />
+            </Text>
+
+            <Style.Wrapper>
+              <Style.HalfWrapper>
+                <CustomInput
+                  error=""
+                  hint=""
+                  label="나이"
+                  placeholder="예) 4"
+                  variant="outlined"
+                />
+                <Text color="gray100" typo="body100">
+                  살
+                </Text>
+              </Style.HalfWrapper>
+
+              <Style.HalfWrapper>
+                <CustomInput
+                  error=""
+                  hint=""
+                  label="몸무게"
+                  placeholder="예) 22"
+                  variant="outlined"
+                />
+                <Text color="gray100" typo="body100">
+                  kg
+                </Text>
+              </Style.HalfWrapper>
+            </Style.Wrapper>
+
+            <DatePicker
+              yearOptions={yearOptions.map((year) => year.toString())}
+              monthOptions={monthOptions.map((month) => month.toString())}
+              dayOptions={dayOptions.map((day) => day.toString())}
+              selectedYear={selectedYear}
+              selectedMonth={selectedMonth}
+              selectedDay={selectedDay}
+              onYearSelect={handleYearSelect}
+              onMonthSelect={handleMonthSelect}
+              onDaySelect={handleDaySelect}
+            />
+
+            <Text typo="subtitle300">
+              질병 이력 <Text typo="body400">(중복 선택도 가능해요)</Text>
+              <MultiSelectButton
+                {...(LargeGrid.args as MultiSelectButtonProps)}
+                selectedIndexes={[0]}
+                onSelect={handleDiseaseSelect}
+              />
+            </Text>
+
             <CustomInput
               error=""
               hint=""
-              label="이름"
-              placeholder="이름을 입력해주세요"
+              label="기타"
+              placeholder="기타 건강상태를 알려주세요"
               variant="outlined"
             />
-          </Style.Wrapper>
 
-          <DropButton
-            label="견종"
-            placeholder="견종을 선택해주세요"
-            options={dogBreeds}
-            onSelect={handleBreedSelect}
-          />
-          <Text typo="subtitle300">
-            성별
-            <RadioSelectButton
-              {...(Gender.args as RadioSelectButtonProps)}
-              selectedIndex={0}
-              onSelect={handleGenderSelect}
+            <CustomInput
+              error=""
+              hint=""
+              label="참고"
+              placeholder="미용 시 참고해야 할 내용이 있다면 알려주세요"
+              variant="outlined"
             />
-          </Text>
-
-          <Text typo="subtitle300">
-            분류
-            <RadioSelectButton
-              {...(Default.args as RadioSelectButtonProps)}
-              selectedIndex={0}
-              onSelect={handleSizeSelect}
-            />
-          </Text>
-
-          <Style.Wrapper>
-            <Style.HalfWrapper>
-              <CustomInput
-                error=""
-                hint=""
-                label="나이"
-                placeholder="예) 4"
-                variant="outlined"
-              />
-              <Text color="gray100" typo="body100">
-                살
-              </Text>
-            </Style.HalfWrapper>
-
-            <Style.HalfWrapper>
-              <CustomInput
-                error=""
-                hint=""
-                label="몸무게"
-                placeholder="예) 22"
-                variant="outlined"
-              />
-              <Text color="gray100" typo="body100">
-                kg
-              </Text>
-            </Style.HalfWrapper>
-          </Style.Wrapper>
-
-          <DatePicker
-            yearOptions={yearOptions.map((year) => year.toString())}
-            monthOptions={monthOptions.map((month) => month.toString())}
-            dayOptions={dayOptions.map((day) => day.toString())}
-            selectedYear={selectedYear}
-            selectedMonth={selectedMonth}
-            selectedDay={selectedDay}
-            onYearSelect={handleYearSelect}
-            onMonthSelect={handleMonthSelect}
-            onDaySelect={handleDaySelect}
-          />
-
-          <Text typo="subtitle300">
-            질병 이력 <Text typo="body400">(중복 선택도 가능해요)</Text>
-            <MultiSelectButton
-              {...(LargeGrid.args as MultiSelectButtonProps)}
-              selectedIndexes={[0]}
-              onSelect={handleDiseaseSelect}
-            />
-          </Text>
-
-          <CustomInput
-            error=""
-            hint=""
-            label="기타"
-            placeholder="기타 건강상태를 알려주세요"
-            variant="outlined"
-          />
-
-          <CustomInput
-            error=""
-            hint=""
-            label="참고"
-            placeholder="미용 시 참고해야 할 내용이 있다면 알려주세요"
-            variant="outlined"
-          />
-          <Style.ConfirmButtonWrapper>
-            <CustomButton fullwidth variant="primary">
-              확인
-            </CustomButton>
-          </Style.ConfirmButtonWrapper>
-        </Style.EditPageWrapper>
+          </Style.EditPageWrapper>
+        </Wrapper>
+        <Style.ConfirmButtonWrapper>
+          <CustomButton fullwidth variant="primary">
+            확인
+          </CustomButton>
+        </Style.ConfirmButtonWrapper>
       </Layout>
     </>
   );
