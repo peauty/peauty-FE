67794ace1bae6e17b8d89cb84c4dd75594e64b91
ZUITOPIA <korhwngml123@naver.com>
2024-11-29 19:13:45 +0900

    style: overflow 시 auto 적용하여 스크롤되게 설정 및 프로필 수정 카메라 아이콘 위치 roundImg를 기준으로 재설정

--- src/components/global/ProfileImg/ProfileImg.styles.ts
+++ src/components/global/ProfileImg/ProfileImg.styles.ts
@@ -12,20 +12,21 @@ export const StyleProfileImg = {
     width: 100%;
     display: flex;
     justify-content: center;
+    position: relative;
   `,
   RoundImg: styled.img<StyleImg>`
-    position: relative;
     width: ${(props) => (props.width ? props.width : "100%")};
     height: ${(props) => (props.height ? props.height : "auto")};
     border: ${(props) => (props.border ? props.border : 0)};
     border-radius: 100%;
     cursor: ${(props) => props.pointer && "pointer"};
+    position: relative;
   `,
   EditButton: styled.button`
     position: absolute;
+    bottom: -10px;
+    right: 140px;
     padding: 5px;
     cursor: pointer;
-    top: 150px;
-    left: 260px;
   `,
 };
--- src/components/global/layout/Wrpper/Wrapper.styles.ts
+++ src/components/global/layout/Wrpper/Wrapper.styles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const WrapperStyle = styled.div`
   width: 100%;
   padding: 0 25px;
-  height: 100vh;
+  height: 100%;
+  overflow: auto;
 `;
