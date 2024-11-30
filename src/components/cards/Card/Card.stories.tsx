57166fcf2e1d5d48dc77070bb43dc486afff9a54
myoungjinGo-FE <cokemania2099@gmail.com>
2024-11-30 21:32:57 +0900

    feat: 카드 컴포넌트 생성 및 태그 컴포넌트 수정

---
+++ src/components/cards/Card/Card.stories.tsx
@@ -0,0 +1,65 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Card from "./Card";
+import { styled } from "styled-components";
+import theme from "../../../style/theme";
+import { TagProps } from "../../category/Tag/Tag";
+
+const meta: Meta<typeof Card> = {
+  title: "Components/Card",
+  component: Card,
+  tags: ["autodocs"], // 자동으로 문서화를 활성화
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+// 예제 TagProps 생성
+const exampleTagProps: TagProps = {
+  color: {
+    backgroundColor: "blue300",
+    borderColor: "blue100",
+    fontColor: "blue100",
+  },
+};
+
+export const Default: Story = {
+  args: {
+    imageSrc: "https://via.placeholder.com/80",
+    name: "꼬미",
+    age: 3,
+    gender: "암컷",
+    weight: "3.4kg",
+    breed: "말티즈",
+    tags: [
+      { text: "피부병", tagProps: exampleTagProps },
+      { text: "슬개골", tagProps: exampleTagProps },
+    ],
+  },
+};
+
+// 전역 스타일 wrapper
+const _globalStyleWidth = styled.div`
+  max-width: ${theme.size.maxWidth};
+  min-width: ${theme.size.minWidth};
+`;
+
+export const WithWrapper: Story = {
+  render: (args) => (
+    <_globalStyleWidth>
+      <Card {...args} />
+    </_globalStyleWidth>
+  ),
+  args: {
+    imageSrc: "https://via.placeholder.com/80",
+    name: "꼬미",
+    age: 3,
+    gender: "암컷",
+    weight: "3.4kg",
+    breed: "말티즈",
+    tags: [
+      { text: "피부병", tagProps: exampleTagProps },
+      { text: "슬개골", tagProps: exampleTagProps },
+    ],
+  },
+};
--- src/components/category/Tag/Tag.styles.ts
+++ src/components/category/Tag/Tag.styles.ts
@@ -5,18 +5,19 @@ import { colors } from "../../../style/color";
 
 interface TagStyleProps {
   color?: {
-    backgroundColor?: string;
-    borderColor?: string;
-    fontColor?: string;
+    backgroundColor?: keyof typeof colors;
+    borderColor?: keyof typeof colors;
+    fontColor?: keyof typeof colors;
   };
 }
 
 export const TagWrapper = styled.div<TagStyleProps>`
-  color: ${({ color }) => color?.fontColor || colors.blue200}; /* 폰트 색상 */
-  border: 1px solid ${({ color }) => color?.borderColor || colors.blue200}; /* border 색상 */
-  border-radius: 5px;
-  background-color: ${({ color }) =>
-    color?.backgroundColor || colors.blue100}; /* 배경 색상 */
   padding: 3px 10px;
-  font-weight: 500;
+  ${typography.body300};
+  border-radius: 5px;
+  text-align: center;
+
+  /* 색상 관련 스타일링 */
+  background-color: ${({ color }) => color?.backgroundColor ? colors[color.backgroundColor] : colors.blue300};
+  border: 1px solid ${({ color }) => color?.borderColor ? colors[color.borderColor] : colors.gray300};
 `;
