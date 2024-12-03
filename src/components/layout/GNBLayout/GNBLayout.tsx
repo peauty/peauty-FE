544719262c480f7f789142ca60d315b8e98fe431
myoungjinGo-FE <cokemania2099@gmail.com>
2024-12-03 17:39:14 +0900

    fix: GNB 컴포넌트 수정

--- src/components/layout/GNBLayout/GNBLayout.tsx
+++ src/components/layout/GNBLayout/GNBLayout.tsx
@@ -1,20 +1,15 @@
-import { CSSProperties, ReactNode } from "react";
-import { Main, Wrapper } from "../Layout/Layout.styles";
 import GNB from "../GNB/GNB";
 import { Outlet } from "react-router-dom";
 
 interface LayoutProps {
-  style?: CSSProperties;
   type?: "user" | "stylist";
 }
 
-export default function GNBLayout({ style, type }: LayoutProps) {
+export default function GNBLayout({ type }: LayoutProps) {
   return (
-    <Wrapper>
-      <Main style={style}>
-        <Outlet/>
-      </Main>
+    <>
+      <Outlet/>
       <GNB type={type}/>
-    </Wrapper>
+    </>
   );
 }
