9826464125a3aef6e9473defdc8ba176c833362b
myoungjinGo-FE <cokemania2099@gmail.com>
2024-12-04 13:36:34 +0900

    feat: 스토리 파일 생성

--- src/components/button/LocationButton/LocationButton.stories.tsx
+++ src/components/button/LocationButton/LocationButton.stories.tsx
@@ -1,1 +1,21 @@
-import { LocationButton } from "./index";
+import { Meta, StoryObj } from "@storybook/react";
+import LocationButton from "./LocationButton";
+
+const meta: Meta<typeof LocationButton> = {
+  title: "Components/LocationButton",
+  component: LocationButton,
+  tags: ["autodocs"],
+  parameters: {
+    controls: {
+      expanded: true,
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof LocationButton>;
+
+export const Default: Story = {
+  args: {},
+};
