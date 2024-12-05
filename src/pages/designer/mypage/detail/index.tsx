e92b3b23da33cb9fe59ba1f54b9542feab3a1e68
jissssu <leeland21@naver.com>
2024-12-05 15:34:42 +0900

    fix: GNB type 오류 수정

--- src/pages/designer/mypage/detail/index.tsx
+++ src/pages/designer/mypage/detail/index.tsx
@@ -66,7 +66,7 @@ export default function CustomerMyPageDetail() {
           <Divider />
         </FieldWrapper>
       </PageWrapper>
-      <GNB type="customer" />
+      <GNB type="designer" />
     </>
   );
 }
