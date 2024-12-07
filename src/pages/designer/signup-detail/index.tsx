98ede031bb0a4a5d8bbf47ca08ac0d6c7e29d894
ZUITOPIA <korhwngml123@naver.com>
2024-12-07 21:00:09 +0900

    refactor: 자격증 입력받는 부분 컴포넌트로 분리

--- src/pages/designer/signup-detail/index.tsx
+++ src/pages/designer/signup-detail/index.tsx
@@ -7,6 +7,7 @@ import {
   Text,
 } from "../../../components";
 import { TitleContentInput } from "../../../components/input/TitleContentInput";
+import { CertificateInputSection } from "./components/CertificateInputSection";
 import { ShopInfoInputSection } from "./components/ShopInfoInputSection";
 import { Style } from "./index.styles";
 
@@ -61,20 +62,7 @@ export default function DesignerSignUpDetail() {
         </Style.SectionWrapper>
 
         <Style.SectionWrapper>
-          <Style.TitleWrapper>
-            <Text typo="subtitle300">자격증 및 기타서류</Text>
-            <Text color="gray100" typo="body500">
-              자격증을 등록하면 고객에게 신뢰를 줄 수 있어요
-            </Text>
-          </Style.TitleWrapper>
-          <CustomButton variant="outline">
-            <Style.ColumnWrapper>
-              <Text color="blue100" typo="body500">
-                증빙 사진을 업로드 해주세요
-              </Text>
-              <AddImage width={15} />
-            </Style.ColumnWrapper>
-          </CustomButton>
+          <CertificateInputSection />
         </Style.SectionWrapper>
       </Style.RegisterPageWrapper>
       <GNB buttonText="확인" />
