51ad68fb777ee3befaed088039a48a558f1c8940
jissssu <leeland21@naver.com>
2024-12-07 21:51:18 +0900

    fix: step2,3 inputData 추가

--- src/pages/customer/pet/signup/components/Step/step3.tsx
+++ src/pages/customer/pet/signup/components/Step/step3.tsx
@@ -1,37 +1,38 @@
-//Step3
 import { CustomInput } from "../../../../../../components/input/CustomInput";
 import { CustomButton } from "../../../../../../components/button/CustomButton";
-import { ConfirmButtonWrapper ,SectionWrapper} from "../../index.styles";
-import { Text } from "../../../../../../components/texts/Text"; 
+import { SectionWrapper } from "../../index.styles";
+import { Text } from "../../../../../../components/texts/Text";
+import { signUpCustomHook } from "../../../../../../hooks/signUpCustomHook";
 
 interface Step3Props {
   onSubmit: () => void;
 }
 
 export default function Step3({ onSubmit }: Step3Props) {
+  const { inputData, handleChange } = signUpCustomHook();
+
   return (
     <div>
-        <SectionWrapper>
+      <SectionWrapper>
         <Text color="black100" typo="subtitle100">
-        미용 시 주의할 점이 있다면 알려주세요
-      </Text>
-      <Text color="blue100" typo="body400">
-        성격, 싫어하는 행동, 평소 미용 스트레스 정도 등
-      </Text>
+          미용 시 주의할 점이 있다면 알려주세요
+        </Text>
+        <Text color="blue100" typo="body400">
+          성격, 싫어하는 행동, 평소 미용 스트레스 정도 등
+        </Text>
 
-   
-      <CustomInput
-        label="참고"
-        placeholder="참고할 점을 알려주세요"
-        fullwidth={true}
-        variant="outlined"
-      />
-   
-      <ConfirmButtonWrapper>
+        <CustomInput
+          label="참고"
+          placeholder="참고할 점을 알려주세요"
+          fullwidth={true}
+          variant="outlined"
+          value={inputData.detail}  
+          onChange={(event) => handleChange(event, "detail")} 
+        />
+
         <CustomButton size="medium" onClick={onSubmit}>
           확인
         </CustomButton>
-      </ConfirmButtonWrapper>
       </SectionWrapper>
     </div>
   );
