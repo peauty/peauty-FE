320832c3eda151bc5e94026173aaddb2f866cc43
jissssu <leeland21@naver.com>
2024-12-07 21:51:18 +0900

    fix: step2,3 inputData 추가

--- src/pages/customer/pet/signup/components/Step/step2.tsx
+++ src/pages/customer/pet/signup/components/Step/step2.tsx
@@ -1,53 +1,62 @@
+import { signUpCustomHook } from "../../../../../../hooks/signUpCustomHook";
 import { MultiSelectButton } from "../../../../../../components/button/MultiSelectButton";
 import { CustomInput } from "../../../../../../components/input/CustomInput";
 import { CustomButton } from "../../../../../../components/button/CustomButton";
-import { ConfirmButtonWrapper ,SectionWrapper} from "../../index.styles";
-import { Text } from "../../../../../../components/texts/Text"; 
+import {  SectionWrapper } from "../../index.styles";
+import { Text } from "../../../../../../components/texts/Text";
+import { AddPuppyRequest } from "../../../../../../types/customer/puppy";
+
 
 interface Step2Props {
   onNext: () => void;
+  inputData: AddPuppyRequest;
 }
 
 export default function Step2({ onNext }: Step2Props) {
+  const { inputData, updateDisease, updateDiseaseDescription } = signUpCustomHook();
+
+  const handleDiseaseSelect = (selectedIndexes: number[]) => {
+    const diseases = [
+      "없음", "외이염", "슬개골", "CANINE_INFLUENZA", "HEART_WORM", "PARVOVIRUS", "RABIES", "기타",
+    ];
+    const selectedDiseases = selectedIndexes.map((index) => diseases[index]);
+    updateDisease(selectedDiseases);
+  };
+
+  const handleDiseaseDescriptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    updateDiseaseDescription(event.target.value);
+  };
+
   return (
-    <div>
-      <SectionWrapper>
+    <SectionWrapper>
       <Text color="black100" typo="subtitle100">
         반려견의 건강상태를 알려주세요
       </Text>
-  
+
       <Text color="blue100" typo="body100">
         질병 이력 (중복 선택도 가능해요)
       </Text>
+
       <MultiSelectButton
         row={3}
         col={3}
-        buttonNames={[
-          "없음",
-          "질병1",
-          "질병2",
-          "질병3",
-          "질병4",
-          "질병5",
-          "질병6",
-          "기타",
-        ]}
-        selectedIndexes={[]}
-        onSelect={() => {}}
+        buttonNames={["없음", "슬개골", "KENNEL_COUGH", "CANINE_INFLUENZA", "HEART_WORM", "PARVOVIRUS", "RABIES", "기타"]}
+        selectedIndexes={[]} // 선택된 질병 인덱스
+        onSelect={handleDiseaseSelect}
       />
+
       <CustomInput
         label="기타"
         placeholder="기타 건강상태를 알려주세요"
         fullwidth={true}
         variant="outlined"
+        value={inputData.diseaseDescription}
+        onChange={handleDiseaseDescriptionChange}
       />
-     
-      <ConfirmButtonWrapper>
-        <CustomButton fullwidth onClick={onNext}>
-          다음
-        </CustomButton>
-      </ConfirmButtonWrapper>
-      </SectionWrapper>
-    </div>
+
+      <CustomButton fullwidth onClick={onNext}>
+        다음
+      </CustomButton>
+    </SectionWrapper>
   );
 }
