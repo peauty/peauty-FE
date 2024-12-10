a1f4a24ab6f1f2db8cd24c17f2ba523f0fbabb1c
ZUITOPIA <korhwngml123@naver.com>
2024-12-10 18:51:50 +0900

    feat: appbar, gnb 등록

---
+++ src/pages/customer/review/write-review/index.tsx
@@ -0,0 +1,11 @@
+import { AppBar, GNB } from "../../../../components";
+
+export default function WriteReview() {
+  return (
+    <>
+      <AppBar prefix="backButton" title="리뷰 작성" />
+
+      <GNB buttonText="등록하기" />
+    </>
+  );
+}
