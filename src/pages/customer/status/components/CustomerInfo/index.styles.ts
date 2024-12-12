87e6aee5ea9546c50627d2779de7547f59e6ed10
ZUITOPIA <korhwngml123@naver.com>
2024-12-12 19:02:50 +0900

    feat: 리뷰 컴포넌트 수정

--- src/pages/customer/status/components/CustomerInfo/index.styles.ts
+++ src/pages/customer/status/components/CustomerInfo/index.styles.ts
@@ -28,56 +28,49 @@ export const InfoWrapper = styled.div`
   flex-direction: column;
   line-height: 1.3;
   gap: 3px; // 내부 요소들 사이의 간격
+  width: 100%;
 `;
 
 export const NameWrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 225px; // name과 status 사이 간격
+  justify-content: space-between;
 `;
 
 export const RatingWrapper = styled.div`
   display: flex;
   align-items: center;
-
-  // Rating과 review는 기본 간격 유지
-  & > :first-child {
-    margin-right: 4px; // Rating과 review 사이 작은 간격
-  }
-
-  // review와 payment 사이 간격을 200px로 설정
-  & > :nth-child(2) {
-    margin-right: 230px;
-  }
+  justify-content: space-between;
 `;
+
+export const RatingAndReviewWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
 export const StoreReservationWrapper = styled.div`
   display: flex;
   align-items: center;
-  gap: 150px; 
+  justify-content: space-between;
 `;
 export const DateWrapper = styled.div`
   font-size: 15px;
   color: ${colors.gray100};
-  //padding: 0 30px;
- // margin: 5px 0; // 상하 간격을 약간 추가
 `;
-
-const DividerWrapper = styled.div`
-  padding: 0; /* 내부 간격 제거 */
-  margin: 0; /* 외부 간격 제거 */
-`;
-
-
-
 
 export const ButtonWrapper = styled.div`
   display: flex;
-  gap: 5px;
-  justify-content: center;
+  gap: 10px;
   margin-top: 10px;
 `;
 
-export const StyledButton = styled.button<{ bgColor: string; color: string; width: string; height?: string }>`
+export const StyledButton = styled.button<{
+  bgColor: string;
+  color: string;
+  width: string;
+  height?: string;
+}>`
   background-color: ${(props) => props.bgColor};
   color: ${(props) => props.color};
   width: ${(props) => props.width};
