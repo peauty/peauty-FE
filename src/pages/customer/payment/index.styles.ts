7784d5935c141dce50132d4ce435bdc81bbb9b1f
jissssu <leeland21@naver.com>
2024-12-13 01:19:46 +0900

    feat: payment 페이지 ui 생성

---
+++ src/pages/customer/payment/index.styles.ts
@@ -0,0 +1,49 @@
+import { styled } from "styled-components";
+
+export const StoreWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 10px;
+  margin-bottom: 15px; 
+`;
+
+export const PaymentDayWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 27px;
+  margin-bottom: 15px; 
+`;
+
+export const PaymenWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 40px; 
+`;
+
+export const InfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px; 
+  margin-bottom: 17px; 
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  margin-top: 20px;
+  bottom: 20px;
+`;
+
+export const ContentWrapper = styled.div`
+  padding: 20px; 
+  
+  
+`;
+
+export const CenteredSvgWrapper = styled.div`
+  display: flex;
+  justify-content: center; 
+  margin-top: 40px; 
+`;
---
+++ src/pages/customer/payment/index.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { AppBar, Text, Divider, CustomButton, GNB } from "../../../components";
+import SvgPayment from "../../../assets/svg/Payment";
+import { StoreWrapper, PaymentDayWrapper, PaymenWrapper, InfoWrapper, ButtonWrapper, ContentWrapper, CenteredSvgWrapper } from "./index.styles";
+import SvgCircle from "../../../assets/svg/Circle";
+
+export default function Payment() {
+  const [paymentData] = useState({
+    storeName: "몽크의 아틀리에 위례점",
+    paymentDate: "2024.11.29 11:04:32",
+    paidAmount: "15,000원",
+    onSiteAmount: "35,000원",
+  });
+
+  return (
+    <>
+      <AppBar prefix="backButton" title="결제완료" />
+      
+      <ContentWrapper>
+        <Text typo="title200" className="padded-text">예약금 결제가 <br />완료되었습니다.</Text>
+        <CenteredSvgWrapper>
+          <SvgPayment width={110} />
+        </CenteredSvgWrapper>
+      </ContentWrapper>
+      
+      <Divider thickness={1} />
+
+      <StoreWrapper>
+        <Text typo="subtitle200" color="gray100">상호명</Text>
+        <Text typo="subtitle200">{paymentData.storeName}</Text>
+      </StoreWrapper>
+
+      <PaymentDayWrapper>
+        <Text typo="subtitle200" color="gray100">결제일자</Text>
+        <Text typo="subtitle200">{paymentData.paymentDate}</Text>
+      </PaymentDayWrapper>
+
+      <PaymenWrapper>
+        <Text typo="subtitle200" color="gray100">결제금액</Text>
+        <Text typo="subtitle200" color="blue100">{paymentData.paidAmount}</Text>
+      </PaymenWrapper>
+
+      <Divider thickness={1} />
+      
+      <PaymenWrapper>
+        <Text typo="subtitle200" color="gray100">현장 결제 예정 금액</Text>
+        <Text typo="subtitle200" color="blue100">{paymentData.onSiteAmount}</Text>
+      </PaymenWrapper>
+      
+      <InfoWrapper>
+        <SvgCircle width={5} />
+        <Text typo="body200" color="gray200">
+          현장금액 결제는 예약금을 제외한 나머지 금액이 결제됩니다.
+        </Text>
+      </InfoWrapper>
+
+      <InfoWrapper>
+        <SvgCircle width={5} />
+        <Text typo="body200" color="gray200">
+          예약금에 해당하는 결제금액은 즉시 결제됩니다.
+        </Text>
+      </InfoWrapper>
+
+      <InfoWrapper>
+        <SvgCircle width={5} />
+        <Text typo="body200" color="gray200">
+          결제 취소를 원하는 경우 요청현황 미용 완료함에서 확인이 가능합니다.
+        </Text>
+      </InfoWrapper>
+
+      <ButtonWrapper>
+        <CustomButton fullwidth>
+          다음
+        </CustomButton>
+      </ButtonWrapper>
+    </>
+  );
+}
