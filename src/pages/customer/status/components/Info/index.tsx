cfcd1274c0d61d0c6c89a2d7166b0fec0eacf1ad
sieon <sieon.eve@gmail.com>
2024-12-12 14:22:49 +0900

    chore: Info.stlyes.ts 공백제거

--- src/pages/customer/status/components/Info/index.tsx
+++ src/pages/customer/status/components/Info/index.tsx
@@ -1,10 +1,9 @@
-
 import {
   Container,
   InnerBox,
   CutTextWrapper,
   RequestButton,
-}  from "./index.styles";
+} from "./index.styles";
 
 import { Text } from "../../../../../components";
 
@@ -16,15 +15,18 @@ export default function Info() {
   return (
     <Container>
       <InnerBox>
-      
-        <Text typo="body300" color="blue100">20024.12.24</Text>
+        <Text typo="body300" color="blue100">
+          20024.12.24
+        </Text>
         <CutTextWrapper>
-          <Text typo="subtitle200"  color="black">곰돌이컷+가위컷</Text>
-          <RequestButton onClick={handleButtonClick}>내 요청 보기</RequestButton>
+          <Text typo="subtitle200" color="black">
+            곰돌이컷+가위컷
+          </Text>
+          <RequestButton onClick={handleButtonClick}>
+            내 요청 보기
+          </RequestButton>
         </CutTextWrapper>
       </InnerBox>
     </Container>
-     );
-    }
-    
-  
+  );
+}
