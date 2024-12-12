4c60da947e8ab1ade17d04e9216a4bf625c2b97b
ZUITOPIA <korhwngml123@naver.com>
2024-12-12 16:47:16 +0900

    feat: 리뷰 tab 생성

--- src/pages/customer/review/history/index.tsx
+++ src/pages/customer/review/history/index.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import { AppBar, GNB } from "../../../../components";
+import ReviewTabs from "./components/ReviewTabs";
+
+type Tab = "new" | "history";
 
 export default function ReviewHistory() {
+  const [activeTab, setActiveTab] = useState<Tab>("new");
   return (
     <>
       <AppBar prefix="backButton" title="리뷰 내역" />
-
+      <ReviewTabs activeTab={activeTab} onTabChange={setActiveTab} />
       <GNB type="customer" />
     </>
   );
