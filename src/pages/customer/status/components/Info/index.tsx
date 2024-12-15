ebfe8bf312a251aa78de0ae35298ecd8c2e3c8e4
jissssu <leeland21@naver.com>
2024-12-16 02:11:05 +0900

    feat: info 컴포넌트 props 및 버튼 클릭시 navigate 추가

--- src/pages/customer/status/components/Info/index.tsx
+++ src/pages/customer/status/components/Info/index.tsx
@@ -7,12 +7,18 @@ import {
 } from "./index.styles";
 import { useNavigate } from "react-router-dom";
 import { Text } from "../../../../../components";
-import { colors } from "../../../../../style/color";
 
-export default function Info() {
+
+interface InfoProps {
+  requestDate: string;
+  requestText: string;
+}
+
+export default function Info({ requestDate, requestText }: InfoProps) {
   const navigate = useNavigate();
+
   const handleButtonClick = () => {
-    navigate("/customer/request/view");
+    navigate("/customer/request/request-look");
   };
 
   return (
@@ -20,11 +26,11 @@ export default function Info() {
       <InnerBox>
         <Text typo="body300" color="gray100">
           <RequestDate>요청일</RequestDate>
-          2024.12.24
+          {requestDate}
         </Text>
         <CutTextWrapper>
           <Text typo="subtitle300" color="blue100">
-            곰돌이컷+가위컷
+            {requestText}
             <Text typo="subtitle300">을 요청했어요!</Text>
           </Text>
           <RequestButton onClick={handleButtonClick}>
