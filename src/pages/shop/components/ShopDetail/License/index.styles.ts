64292919b88563bd12e5f7232955199196f4ff1d
ZUITOPIA <korhwngml123@naver.com>
2024-12-19 20:43:50 +0900

    fix: 고객 시점 가게 정보 뷰 수정

--- src/pages/shop/components/ShopDetail/License/index.styles.ts
+++ src/pages/shop/components/ShopDetail/License/index.styles.ts
@@ -1,13 +1,13 @@
 import styled from "styled-components";
-import { colors } from "../../../../../style/color";
 
 export const ImageWrapper = styled.div`
   display: flex;
   margin-top: 15px;
-  gap: 5px;
+  justify-content: space-between;
+  gap: 8px;
 `;
 
 export const ImageItem = styled.div`
-  width: 85px;
-  height: 80px;
+  width: 40px;
+  height: 130px;
 `;
