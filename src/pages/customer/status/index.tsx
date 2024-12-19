54c24857276d6debc92cbfd1bd2d8c66c0e19750
jissssu <leeland21@naver.com>
2024-12-19 11:39:36 +0900

    feat: status에 견적 없는 경우 컴포넌트 추가

--- src/pages/customer/status/index.tsx
+++ src/pages/customer/status/index.tsx
@@ -19,6 +19,8 @@ import {
   GetOngoingProcessWithStep2ThreadsResponse,
 } from "../../../types/customer/bidding";
 import Basic from "../../../assets/images/basic.png";
+import NoReceived from "./components/NoReceived";
+import Nosend from "./components/Nosent";
 interface StatusItemData {
   name: string;
   store: string;
@@ -146,24 +148,31 @@ export default function Status() {
       case "sent":
         return (
           <>
-            <Info
-              requestDate={statusItemData?.date || "알 수 없음"}
-              requestText={statusItemData?.name || "요청 내용 없음"}
-              userId={0}
-              puppyId={0}
-              processId={0}
-            />
-            <StatusListItem
-              location={statusItemData?.location || "알 수 없음"}
-              store={statusItemData?.store || "알 수 없음"}
-              score={statusItemData?.score || 0}
-              review={statusItemData?.review || 0}
-              badges={statusItemData?.badges || []}
-              thumbnailUrl={statusItemData?.thumbnailUrl || Basic}
-              onClick={() => console.log("StatusListItem clicked")}
-            />
+            {statusItemData ? (
+              <>
+                <Info
+                  requestDate={statusItemData.date }
+                  requestText={statusItemData.name }
+                  userId={0}
+                  puppyId={0}
+                  processId={0}
+                />
+                <StatusListItem
+                  location={statusItemData.location }
+                  store={statusItemData.store }
+                  score={statusItemData.score }
+                  review={statusItemData.review }
+                  badges={statusItemData.badges }
+                  thumbnailUrl={statusItemData.thumbnailUrl}
+                  onClick={() => console.log("StatusListItem clicked")}
+                />
+              </>
+            ) : (
+              <Nosend />
+            )}
           </>
         );
+      
       case "received":
         return (
           <>
@@ -192,7 +201,7 @@ export default function Status() {
                 />
               ))
             ) : (
-              <p>확인된 데이터가 없습니다.</p>
+              <NoReceived /> 
             )}
           </>
         );
@@ -221,7 +230,7 @@ export default function Status() {
                   />
                 ))
               ) : (
-                <p>확인된 데이터가 없습니다.</p>
+                <NoReceived /> 
               )}
             </div>
           </>
