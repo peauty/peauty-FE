a9d3d60f94d6554e2ced12f817b6312a6db9a6ed
sieon <sieon.eve@gmail.com>
2024-12-28 04:18:53 +0900

    fix: 스크롤 임시 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,10 @@ import { router } from "./router/router";
 import { RouterProvider } from "react-router-dom";
 import GlobalStyle from "./style/global-style";
 import { RecoilRoot } from "recoil";
-import ScrollToTop from "./style/ScrollToTop";
+
 createRoot(document.getElementById("root")!).render(
   <RecoilRoot>
     <GlobalStyle />
-    <ScrollToTop />
     <RouterProvider router={router} />
   </RecoilRoot>,
 );
